abd78ed8b10ffd8bb313798109ff592fa27c8be6
Jonathan Phair <jmphair@gmail.com>
2023-03-06 17:34:34 -0500

    implement load and update

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+puts "---- First Store ----"
+puts @store1.id 
+
+@store2 = Store.find_by(id: 2)
+puts "---- Second Store ----"
+puts @store2.id
+
+@store1.update(name: "Van City")
