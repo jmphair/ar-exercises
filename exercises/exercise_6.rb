c0d5dfba49ec0ef2fa60e9271f64542b1d7c70ad
Jonathan Phair <jmphair@gmail.com>
2023-03-06 18:14:46 -0500

    implement one to many

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Jonathan", last_name: "Phair", hourly_rate: "88")
+@store1.employees.create(first_name: "Queenie", last_name: "Phair", hourly_rate: "99")
+@store1.employees.create(first_name: "Abbie", last_name: "Phair", hourly_rate: "22")
+
+@store2.employees.create(first_name: "Elsa", last_name: "Wong", hourly_rate: "77")
+@store2.employees.create(first_name: "Chinnie", last_name: "Wong", hourly_rate: "66")
+@store2.employees.create(first_name: "Ayla", last_name: "Wong", hourly_rate: "11")
+
+puts "#{@store1.name} has #{@store1.employees.count} employees"
+puts "#{@store2.name} has #{@store2.employees.count} employees"
